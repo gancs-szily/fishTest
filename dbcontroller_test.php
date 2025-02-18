d2446e294a606927cbc124ccc56f24e40949215b
gancs-szily <71602533755@szily.hu>
2025-02-18 11:00:59 +0100

    dbcontroller letesztelve

--- dbcontroller_test.php
+++ dbcontroller_test.php
@@ -6,8 +6,28 @@ error_reporting(E_ALL);
 require_once 'dbcontroller.php';
 $db = new DBController();
 
+//kapcs teszt
 if($db->connectDB()){
     echo "Sikeres kapcs";
 }else{
     echo "Sikertelen kapcs";
 }
+
+//select teszt
+$query="SELECT * FROM fishingrod";
+$eredmeny=$db->executeSelectQuery($query);
+
+if($eredmeny !== "hiba"){
+    echo "<br>";
+    echo "lekérdezés sikeres";
+    echo "<pre>";
+    print_r($eredmeny);
+    echo "<pre>";
+}else{
+    echo "lekérdezés sikertelen";
+    echo "<pre>";
+    print_r($eredmeny);
+    echo "<pre>";
+}
+$db->closeDB();
+echo "lezárva";
